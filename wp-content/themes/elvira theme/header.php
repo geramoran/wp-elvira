8ac4e520c81708352b6acbe10e6719c9dd17969c
Jose Gerardo Martinez Moran <geramtz100@gmail.com>
2019-02-12 02:16:09 -0600

    update header agregar elvirarichards

--- wp-content/themes/elvira theme/header.php
+++ wp-content/themes/elvira theme/header.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
-	<title><?php echo the_title(); ?></title>
+	<title><?php echo the_title() . " - Elvira Richards"; ?></title>
 	<?php wp_head(); ?>	
 </head>
 <body>
